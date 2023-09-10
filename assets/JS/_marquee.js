1a12131c9bee1d8d51c5edaa492dddee4b2acb80
tanukile <backpacker320@gmail.com>
2023-09-10 23:02:17 +0800

    feat: index-online+marquee

---
+++ assets/JS/_marquee.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".mySwiper", {
+  allowTouchMove: false, // 避免駛使用者移動到
+  slidesPerView: 'auto', // 自動調整 slide 呈現數量
+  loop: true, // 重複播放
+  speed: 10000, // 速度
+  // 自動撥放設定
+      autoplay: {
+        delay: 0, // 延遲時間
+        disableOnInteraction: false, // 避免點擊造成跑馬燈失效
+      },
+});
--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+// 自釘 js
+import './assets/JS/_marquee';
